63e503218c110db82026246cf0151e33198fd31b
justrawaccel <public@justrawaccel.dev>
2026-01-30 15:16:43 +0200

    feat: add heading and update LinksSkeleton layout

--- src/components/common/skeletons/LinksSkeleton.tsx
+++ src/components/common/skeletons/LinksSkeleton.tsx
@@ -1,12 +1,18 @@
 import Skeleton from '@/components/common/Skeleton';
 
 const LinksSkeleton = () => (
-  <div className="flex flex-col gap-2">
-    <Skeleton className="h-8 w-full" />
-    <Skeleton className="h-8 w-full" />
-    <Skeleton className="h-8 w-full" />
-    <Skeleton className="h-8 w-full" />
-  </div>
+  <>
+    <div className="flex flex-col gap-2">
+      <h3 className="leading-none font-bold">other links</h3>
+      <div className="ml-3 flex flex-col gap-1">
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+        <Skeleton className="h-4 w-full" />
+      </div>
+    </div>
+  </>
 );
 
 export default LinksSkeleton;
